e3f0a357eee1170dca03735e7081ea9ae5a2b2a3
Ariel3004 <villanuevaariel20.05@gmail.com>
2024-07-19 20:25:33 -0400

    dom,br

---
+++ Js/app.js
@@ -0,0 +1,14 @@
+/*
+    DOMContentLoaded sirve 
+    para controlar que primero 
+    se cargue Todo el documento HTML por ultimo javascript
+*/ 
+document.addEventListener("DOMContentLoaded", function(){
+    // alert:S genera o muestra un mensaje emergente
+    alert("Holis");
+});
+
+function hola(nombre, apellido){
+    console.log(nombre, apellido);
+    //codigo reutilizable
+}
